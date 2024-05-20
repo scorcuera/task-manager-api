768a5d44a59cbe6e265df20362fc0325d424a131
scorcuera <scorcuera6@gmail.com>
2024-05-20 19:42:45 +0200

    Refactor server.js to use db.json instead of data.json

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-import jsonServer from 'json-server';
-
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('data.json');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
@@ -9,5 +8,5 @@ server.use(router);
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
+  console.log('JSON Server is running on port', port);
 });
