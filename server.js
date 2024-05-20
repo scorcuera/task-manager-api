29d1a117bba1a1db5351e3bbe69fa0050ee49dc0
scorcuera <scorcuera6@gmail.com>
2024-05-20 19:11:50 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+import { create, router as _router, defaults } from 'json-server'
+const server = create()
+const router = _router('data.json')
+const middlewares = defaults()
+
+server.use(middlewares)
+server.use(router)
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`)
+})
